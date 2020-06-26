1d64562ee49f2310d6b8aee198e03a23d639f484
Nguyen Tan Dat <dat.nguyen_bk@hcmut.edu.vn>
2020-06-26 16:39:06 +0700

    BUG: wrong address in write_mem() CHECKED: no problem with translate and get_page_table cause free work well TODO: find and fix this bug

--- source_code/src/queue.c
+++ source_code/src/queue.c
@@ -2,35 +2,41 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
+int empty(struct queue_t *q)
+{
 	return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
 	/* TODO: put a new process to queue [q] */
-	if (q->size == MAX_QUEUE_SIZE) {
+	if (q->size == MAX_QUEUE_SIZE)
+	{
 		return;
 	}
 	q->proc[q->size] = proc;
 	q->size++;
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
+struct pcb_t *dequeue(struct queue_t *q)
+{
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if (q->size == 0) {
+	if (q->size == 0)
+	{
 		return NULL;
 	}
 	int maxIdx = 0; // index of max priority task in queue
-	for (int i = 1; i < q->size; i++) {
-		if (q->proc[i]->priority > q->proc[maxIdx]->priority) {
+	for (int i = 1; i < q->size; i++)
+	{
+		if (q->proc[i]->priority > q->proc[maxIdx]->priority)
+		{
 			maxIdx = i;
 		}
 	}
-	struct pcb_t * res = q->proc[maxIdx];
-	q->proc[maxIdx] = q->proc[q->size-1];
+	struct pcb_t *res = q->proc[maxIdx];
+	q->proc[maxIdx] = q->proc[q->size - 1];
 	q->size--;
 	return res;
-}	
-
+}
